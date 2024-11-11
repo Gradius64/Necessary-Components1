c4bc78843289c6693a7ea5204c2af9230007cf5a
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-11 12:50:21 -0600

    Add files via upload

---
+++ Necessary Components.tsx
@@ -0,0 +1,32 @@
+const fetchCandidates = async () => {
+
+    
+
+    const response = await fetch('API_URL_TO_FETCH_CANDIDATES');
+    if (response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+}
+
+const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      
+    }
+
+    const Quiz = () => {
+
+    // Function to save user to local storage
+
+
+
+    
+
+
+
+
+
+  
+    }
+}
